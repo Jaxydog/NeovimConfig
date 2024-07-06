dcb00cba1bb28782cac5689d6cf94a4b4569511d
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-05 20:24:15 -0400

    Add markdown-oxide dependency

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -3,11 +3,26 @@ local lsp = require('lsp-zero')
 require('mason').setup({})
 
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'rust_analyzer' },
+    ensure_installed = { 'lua_ls', 'markdown_oxide', 'rust_analyzer' },
     handlers = {
         lsp.default_setup,
         lua_ls = function()
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        end,
+        markdown_oxide = function()
+            local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities(client_capabilities)
+
+            capabilities.workspace = {
+                didChangeWatchedFiles = {
+                    dynamicRegistration = true,
+                },
+            }
+
+            require('lspconfig').markdown_oxide.setup({
+                on_attach = lsp.on_attach,
+                capabilities = capabilities,
+            })
         end,
         rust_analyzer = function()
             require('lspconfig').rust_analyzer.setup({
