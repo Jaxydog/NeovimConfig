fca10387f7f42ec6755cd895ce4d79509d59d0c0
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-09-11 18:57:37 -0400

    Use conform formatter only if a formatter is available

--- lua/jaxydog/remap.lua
+++ lua/jaxydog/remap.lua
@@ -3,9 +3,11 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>,', require('jaxydog.utility').split_terminal)
 vim.keymap.set('n', '<leader>f', function()
-    vim.lsp.buf.format()
-
     local success, conform = pcall(require, 'conform')
 
-    if success then conform.format() end
+    if success and #conform.list_formatters(0) ~= 0 then
+        conform.format()
+    else
+        vim.lsp.buf.format()
+    end
 end)
