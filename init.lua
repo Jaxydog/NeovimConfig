e5695843ced97f09e3cd157c6e23220095296466
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-12-16 02:05:15 -0500

    Enable catppuccin integration

--- init.lua
+++ init.lua
@@ -19,6 +19,11 @@ require('lazy').setup({
         'catppuccin/nvim',
         name = 'catppuccin',
         priority = 1000,
+        opts = {
+            integrations = {
+                mason = true,
+            },
+        },
     },
     {
         'nvim-treesitter/nvim-treesitter',
