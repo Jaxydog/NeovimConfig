d60279e99836a50d0869ae91e56225c54cea832d
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-03-01 18:00:08 -0500

    Update java configuration for better compatibility

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,20 +1,12 @@
 local root_file_names = { '.git', 'build.gradle', 'gradle.properties', 'gradlew', 'README.md', 'settings.gradle' }
 
-local install_path = require('mason-registry').get_package('jdtls'):get_install_path()
-local server_path = install_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'
-local config_path
-local style_path = vim.fn.stdpath('config') .. '/codestyle/java.xml'
-local data_path = vim.fn.stdpath('cache') .. '/nvim-jdtls/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local paths = {}
 
-if vim.fn.has('unix') == 1 then
-    config_path = install_path .. '/config_linux'
-elseif vim.fn.has('win32') == 1 then
-    config_path = install_path .. '/config_win'
-elseif vim.fn.has('mac') == 1 then
-    config_path = install_path .. '/config_mac'
-else
-    error('Failed to detect operating system')
-end
+paths.share = vim.fs.joinpath(vim.fn.expand('$MASON'), 'share', 'jdtls')
+paths.launcher = vim.fs.joinpath(paths.share, 'plugins', 'org.eclipse.equinox.launcher.jar')
+paths.config = vim.fs.joinpath(paths.share, 'config')
+paths.codestyle = vim.fs.joinpath(vim.fn.stdpath('config'), 'codestyle', 'java.xml')
+paths.cache = vim.fs.joinpath(vim.fn.stdpath('cache'), 'nvim-jdtls', vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'))
 
 local jdtls = require('jdtls')
 
@@ -37,14 +29,12 @@ jdtls.start_or_attach({
         '--add-modules=ALL-SYSTEM',
         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar', server_path,
-        '-configuration', config_path,
-        '-data', data_path,
+        '-jar', paths.launcher,
+        '-configuration', paths.config,
+        '-data', paths.cache,
     },
     extendedClientCapabilities = jdtls.extendedClientCapabilities,
-    flags = {
-        allow_incremental_sync = true,
-    },
+    flags = { allow_incremental_sync = true },
     on_attach = function(_, buffer)
         vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = buffer,
@@ -58,26 +48,14 @@ jdtls.start_or_attach({
     end,
     root_dir = vim.fs.root(0, root_file_names),
     settings = {
-        configuration = {
-            updateBuildConfiguration = 'interactive',
-        },
-        contentProvider = {
-            preferred = 'fernflower',
-        },
-        eclipse = {
-            downloadSources = true,
-        },
+        configuration = { updateBuildConfiguration = 'interactive' },
+        contentProvider = { preferred = 'fernflower' },
+        eclipse = { downloadSources = true },
         format = {
             enabled = true,
-            settings = {
-                profile = style_path,
-            },
+            settings = { profile = paths.codestyle },
         },
-        maven = {
-            downloadSources = true,
-        },
-        signatureHelp = {
-            enabled = true,
-        },
+        maven = { downloadSources = true },
+        signatureHelp = { enabled = true },
     },
 })
