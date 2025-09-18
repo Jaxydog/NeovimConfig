b85377ed58e683cd52192cae7e7ef155a52eec63
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-09-18 17:16:43 -0400

    fix: remove automatic installations for parsers that are bundled with Neovim

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'bash', 'lua', 'markdown', 'markdown_inline', 'regex', 'rust', 'toml', 'vim', 'vimdoc' },
+    ensure_installed = { 'bash', 'regex', 'rust', 'toml' },
     ignore_install = {},
     sync_install = false,
     auto_install = true,
